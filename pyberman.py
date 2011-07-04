36275bf6f792b9ee36847fd0309f99c76eb7c210
Zahar Molodchenko <zmolodchenko@gmail.com>
2011-07-04 19:58:59 +0300

    This is the first trial version, made on the first day. There is already the level viewed and most of the classes are written

--- pyberman.py
+++ pyberman.py
@@ -2,9 +2,23 @@
 #Copyright (C) 2011 PyTeam
 
 """The Pyberman runner."""
+import sys
+import pygame
+import GameObjects
+from pygame.locals import *
+pygame.init()
 
 def main():
-    pass
+    screen = pygame.display.set_mode((800,600),FULLSCREEN)
+    b=GameObjects.Level('D:\\projects\\pyberman\\Maps\\map1.bff')
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: sys.exit()
+        screen.fill(100,100,100)
+        for obj in level.objects:
+            screen.blit(obj.pict,obj.pictrect)
+        pygame.display.flip()
 
 if __name__="__main__":
     main()
