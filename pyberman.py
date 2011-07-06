a77874b0ec997ed9b3d29249e0ce90e9dde3dc75
Zahar Molodchenko <zmolodchenko@gmail.com>
2011-07-06 14:34:07 +0300

    Done functions for searching optimal size of squares, changed some images

--- pyberman.py
+++ pyberman.py
@@ -7,6 +7,7 @@ import sys
 import pygame
 import ctypes
 from pygame.locals import *
+import ctypes
 import GameObjects
 
 
@@ -18,11 +19,11 @@ class Game(object):
     #Todo: implement reading from a config file
     config = {
         'screen': {
-            'width': ctypes.windll.user32.GetSystemMetrics(0), 
-            'height': ctypes.windll.user32.GetSystemMetrics(1),
+            #'width': ctypes.windll.user32.GetSystemMetrics(0), 
+            #'height': ctypes.windll.user32.GetSystemMetrics(1),
             },
         'general': {
-            'framerate': 50,
+            'framerate': 1,
             }
         }
 
@@ -30,7 +31,10 @@ class Game(object):
         """Initializes the game."""
         pygame.init()
         pygame.display.set_caption('Pyberman')
-        self.surface = pygame.display.set_mode((self.config['screen']['width'], self.config['screen']['height']), FULLSCREEN|DOUBLEBUF     |HWSURFACE)
+        #self.surface = pygame.display.set_mode((self.config['screen']['width'], self.config['screen']['height']), FULLSCREEN|DOUBLEBUF     |HWSURFACE)
+        self.surface = pygame.display.set_mode((0,0), FULLSCREEN|DOUBLEBUF     |HWSURFACE)
+        self.height = pygame.display.Info().current_h
+        self.width = pygame.display.Info().current_w
         self.level = None
 
     def __del__(self):
@@ -40,14 +44,14 @@ class Game(object):
     def main_loop(self):
         """Starts the game's main loop."""
         #Todo: load a this actually should be done when users selects the level in the GUI
-        self.level = GameObjects.Level('Maps\\map1.bff')
+        self.level = GameObjects.Level('Maps\\map1.bff',self.height,self.width)
         #to control a framerate
         clock = pygame.time.Clock()
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE): return
             self.level.update()
-            self.surface.fill((100,100,100))
+            #self.surface.fill(pygame.Color.b)
             self.level.draw(self.surface)
             pygame.display.flip()
             #Let other processes to work a bit, limiting the framerate
