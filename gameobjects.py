3cf66dca8f73076e4f8a7f4bddae4d670e4e3964
Aleksey Sadovoy <lex@progger.ru>
2011-07-06 22:09:01 +0300

    refactor class Level into Game (by eliminating it all together). introduce width and height properties. cleanup code a bit

---
+++ gameobjects.py
@@ -0,0 +1,170 @@
+#GameObjects.py
+#Copyright (C) 2011 PyTeam
+
+"""Game objects."""
+
+import os
+import pygame
+
+IMAGE_DIR = "Data"
+
+
+class GameObject(pygame.sprite.Sprite):
+    """The base class for all visible entities in the game, which implements generic operations.
+    See the base class documentation at http://pygame.org/docs/ref/sprite.html#pygame.sprite
+    """
+    #: list of file names of images to load
+    image_files=[]
+
+    def __init__(self, game, y,x, groups=None):
+        self.game = game
+        self.x, self.y = x,y
+        super(GameObject, self).__init__(*(groups if groups is not None else []))
+        self.images=[]
+        if self.image_files:
+            for f in self.image_files:
+                self.images.append(self.load_image(f))
+        if self.images:
+            self.image = self.images[0]
+    
+    def update(self):
+        """Updates the object's state.
+        It is called on each core frame."""
+        if self.image is not None:
+            self.rect = pygame.rect.Rect(self.screen_x, self.screen_y, self.width, self.height)
+
+    @property
+    def width(self):
+        return self.game.side
+
+    @property
+    def height(self):
+        return self.game.side
+    
+    def load_image(self, file_name):
+        return pygame.transform.smoothscale(pygame.image.load(os.path.join(IMAGE_DIR, file_name)), (self.width, self.height))
+
+    @property
+    def screen_x(self):
+        return self.game.xcoord_to_screen(self.x)
+
+    @property
+    def screen_y(self):
+        return self.game.ycoord_to_screen(self.y)
+
+    def collide(self, other):
+        """By default, it searches a method named 'collide_otherclassname' and calls it if exist.
+        for example, if object implements method named 'collide_Player', it would be called when player collides with this object.
+        @returns: whether the movement can be continued
+        @rtype: bool
+        """
+        func = getattr(self, 'collide_%s'%other.__class__.__name__, None)
+        if func is not None:
+            return func(other)  
+
+
+class Bonus(GameObject):
+    """Represents an item which affects the player on collision and then disappears."""
+
+    def collide_Player(self, player):
+        self.affect_player(player)
+        self.kill()
+        return True #Player can move further
+
+    def affect_player(self, player):
+        """Makes something fun with player.
+        Should be overloaded in derived classes."""
+        pass
+
+class SpeedupBonus(Bonus):
+    """Speeds the player up by a factor."""
+    image_files=['1.jpg']
+
+    def affect_player(self, player):
+        player.speed+=1
+
+        
+class AddBombBonus(Bonus):
+    image_files=['1.jpg']
+
+    def affect_player(self, player):
+        player.bombs += 1
+
+        
+class MoveBombsBonus(Bonus) :
+    image_files=['2.jpg']
+
+    def affect_player(self, player):
+        player.can_move_bombs = True
+
+
+class IncreaseRadiusBonus(Bonus):
+    image_files=['2.jpg']
+
+    def affect_player(self, player):
+        player.radius += 1
+
+        
+class BadBonus(Bonus):
+    image_files=['2.jpg']
+    def affect_player(self, player):
+        # here timer is
+        pass
+
+        
+class SpeedDownBonus(BadBonus):
+    
+    def affect_player(self, player):
+        speed_temp = player.speed
+        player.speed = 3
+        #Lex: WTF??? calling a constructor when object is already created?
+        #Zmolodchenko: this is win!
+        super().__init__(self, player) # timer here?
+        player.speed = speed_temp
+
+        
+class ReduceRadiusBonus(BadBonus):
+
+    def affect_player(self, player):
+        radius_temp = player.radius
+        player.radius = 3
+        #and yet another win
+        super().__init__(self, player) 
+        player.radius = radius_temp
+
+        
+class Bomb(GameObject):
+
+    def collide_Player(self, player):
+        #Lex: what is it supposed to do?
+        if player.can_move_bombs:pass
+
+        
+class Wall(GameObject):
+    """An obstacle which player can not get through."""
+    image_files = ['wall.jpg']
+
+    def collide_Player(self, player):
+        return False #player can not move further
+
+
+class Box(Wall):
+    """An obstacle which can be ruined by a bomb explosion."""
+    image_files = ['box.jpg']
+
+    def collide_BombExplosion(self):
+        self.kill()
+        return True
+
+
+class Player(GameObject):
+    """Represents a player in the game."""
+    player_images = ['1.jpg', '2.jpg', '3.jpg']
+    def __init__(self, game, x, y, id, *args, **kwargs):
+        self.id = id
+        self.bombs = 1
+        self.speed=5
+        self.radius=1
+        self.can_move_bombs=False
+        super(Player, self).__init__(game, x,y, *args, **kwargs)
+        self.image = self.load_image(Player.player_images[id])
--- pyberman.py
+++ pyberman.py
@@ -5,11 +5,8 @@
 
 import sys
 import pygame
-import ctypes
 from pygame.locals import *
-import ctypes
-import GameObjects
-import menu
+from gameobjects import *
 
 
 class Game(object):
@@ -33,11 +30,11 @@ class Game(object):
         pygame.init()
         pygame.display.set_caption('Pyberman')
         #self.surface = pygame.display.set_mode((self.config['screen']['width'], self.config['screen']['height']), FULLSCREEN|DOUBLEBUF     |HWSURFACE)
+		# use default resolution
         self.surface = pygame.display.set_mode((0,0), FULLSCREEN|DOUBLEBUF     |HWSURFACE)
-        self.height = pygame.display.Info().current_h
-        self.width = pygame.display.Info().current_w
-        self.level = None
-        self.menu = None
+        self.screen_height = pygame.display.Info().current_h
+        self.screen_width = pygame.display.Info().current_w
+        self.create_groups()
 
     def __del__(self):
         """Deinitializes the game."""
@@ -46,28 +43,63 @@ class Game(object):
     def main_loop(self):
         """Starts the game's main loop."""
         #Todo: load a this actually should be done when users selects the level in the GUI
-        self.level = GameObjects.Level('Maps\\map1.bff',self.height,self.width)
-        self.menu = menu.MainMenu(self.height,self.width)
+        self.load_level('Maps\\map1.bff')
         #to control a framerate
         clock = pygame.time.Clock()
-        menu_bool = True
-        while menu_bool:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE): menu_bool = False
-            self.menu.update()
-            self.menu.draw(self.surface)
-            pygame.display.flip()
-            #Let other processes to work a bit, limiting the framerate
-            clock.tick(self.config['general']['framerate'])
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE): return
-            self.level.update()
-            self.level.draw(self.surface)
+            self.update()
+            self.redraw()
             pygame.display.flip()
             #Let other processes to work a bit, limiting the framerate
             clock.tick(self.config['general']['framerate'])
 
+    def load_level(self, filename):
+        with open(filename) as f:
+            self.height,self.width,self.max_players = [int(x) for x in f.readline().split()]
+            self.side=min((self.screen_height//self.height,self.screen_width//self.width))
+            self._absw = (self.screen_width-(self.width*self.side))//2
+            self._absh = (self.screen_height-(self.height*self.side))//2
+            for row_num, row in enumerate(f):
+                if row_num == self.height: raise RuntimeError('Too many lines in the file')
+                for col_num, col in enumerate(row.strip()):
+                    if col_num == self.width: raise RuntimeError('Too many colums in row %d'%row_num+1)
+                    if col == 'W':
+                        Wall(self, row_num, col_num, groups=(self.all, self.obstacles))
+                    elif col == 'B':
+                        Box(self, row_num, col_num, groups=(self.all, self.dynamic, self.obstacles))
+                    elif col == ' ': 
+                        pass
+                    elif col.isdigit() and int(col)<10:
+                        Player(self, row_num, col_num, int(col)-1, groups=(self.all, self.dynamic))
+                    else:
+                        raise RuntimeError('Unknown symbol "%s" in row %d, col %d'%(col, row_num+1, col_num+1))
+                if col_num<self.width-1:
+                    raise RuntimeError('Insuficient number of colums in row %d'%row_num+1)
+            if row_num<self.height-1:
+                raise RuntimeError('Insuficient number of rows')
+
+    def xcoord_to_screen(self, x):
+        """Translates given x coordinate from the game coord system to screen coord system."""
+        return self._absw+x*self.side
+
+    def ycoord_to_screen(self, y):
+        """Translates given y coordinate from the game coord system to screen coord system."""
+        return self._absh+y*self.side
+
+    def create_groups(self):
+        self.all = pygame.sprite.RenderUpdates()
+        self.obstacles = pygame.sprite.Group()
+        self.dynamic = pygame.sprite.Group() #those objects which may progress over time
+
+    def redraw(self):
+        """Redraws the level. It is called each core pumb"""
+        self.all.draw(self.surface)
+
+    def update(self):
+        self.all.update()
+                
     @classmethod
     def instance(cls):
         if cls._instance is None:
