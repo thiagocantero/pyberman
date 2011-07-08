776089ae1445439875e220a446df22d039867054
Myroslava Stavnycha <gozza.drala@gmail.com>
2011-07-08 19:15:33 +0300

    now we can control our players!

--- controller.py
+++ controller.py
@@ -15,15 +15,23 @@ class LocalController(events.AutoListeningObject):
             K_RIGHT: self.player1.go_right,
             K_SPACE: self.player1.put_bomb,
             K_w:self.player2.go_up,
-            K_a:self.player2.go_down,
-            K_s:self.player2.go_left,
+            K_s:self.player2.go_down,
+            K_a:self.player2.go_left,
             K_d: self.player2.go_right,
             K_LCTRL:self.player2.put_bomb
         }
         events.AutoListeningObject.__init__(self)
         
     def event_keydown(self, event):
-        print 'keydown'
-        self.actions[event.key]()
+        func = self.actions.get(event.key, None)
+        if func is not None:
+            func()
+            event.stop_propagating = True
+        
+    def event_keyup(self, event):
+        if event.key in [K_UP,K_DOWN,K_LEFT,K_RIGHT]: self.player1.dest = None
+        elif event.key in [K_w,K_a,K_s,K_d]: self.player2.dest = None
+        else: event.stop_propagating = True
+        
         
         
